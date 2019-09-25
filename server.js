f783e2b86c24832e2d910fb418cfffb43ee6fb5c
Thariqalfa <zrav420@gmail.com>
2019-09-25 11:05:46 +0700

    update server

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const PORT = 3003;
 const API_VERSION = 'v1';
 const cors = require('cors');
+const redis = require('redis');
 
 // allow request
 app.use(cors());
 
 /* ROUTER */
 const ArticlesRouter = require('./routers/ArticlesRouter');
+// create and connect redis client to local instance.
+const client = redis.createClient(6379)
+const REDIS_KEY = 'NBC';
 
 // Parse incoming requests data
 app.use(express.json());
@@ -18,5 +22,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send('Hello World'));
 app.use('/api/'+ API_VERSION +'/articles', ArticlesRouter);
+app.use('/caches', function(req, res){
+    client.del('NBC');
+
+    // echo redis errors to the console
+	client.on('error', (err) => {
+		console.log("Error " + err)
+	});
+
+    res.json({
+        result: true
+    })
+});
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT));
